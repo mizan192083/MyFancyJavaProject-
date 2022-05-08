d074a16726d1880a4fb2c2038c0092d296adf8a0
mizan192083 <mrahman192083@cse.uiu.ac.bd>
2022-05-09 00:43:29 +0600

    First commit without factory pattern

---
+++ src/sample.java
@@ -0,0 +1,64 @@
+interface Shape
+{
+    void draw();
+}
+
+
+class Circle implements Shape
+{
+    @Override
+    public void draw()
+    {
+        System.out.println("a circle drawn");
+    }
+}
+
+
+class Square implements Shape
+{
+    @Override
+    public void draw()
+    {
+        System.out.println("a square drawn");
+    }
+}
+
+class Rectangle implements Shape
+{
+    @Override
+    public void draw()
+    {
+        System.out.println("a rectangle drawn");
+    }
+}
+
+
+public Shape getShape(String type) throws Exception {
+        switch (type) {
+            case "Circle":
+                return new Circle();
+            case "Square":
+                return new Square();
+            case "Rectangle":
+                return new Rectangle();
+            default:
+                throw new Exception("Shape type : " + type + " cannot be instantiated");
+        }
+}
+
+
+class Painter
+{
+    public static void main(String[] args) throws Exception
+    {
+
+        ShapeFactory shape = new ShapeFactory();
+        Shape circle=shape.getShape("Circle");
+        circle.draw();
+        Shape Rectangle = shape.getShape("Rectangle");
+        square.draw();
+
+        Shape rombus = shape.getShape("Rombus");
+        rombus.draw();
+    }
+}
